d02db52a4ac47763859ddbd9f741641ef91ff3f9
joeyyy688 <edinjoey@gmail.com>
2023-06-04 19:02:58 +0000

    Initial Commit

---
+++ SwiftUI-Networking/SwiftUI_NetworkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NetworkingApp.swift
+//  SwiftUI-Networking
+//
+//  Created by Joseph on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NetworkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
